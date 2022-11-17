79026773991c90b06d4ff3dc9d12b25459ac3221
Drew Phillips <andrewtp32@gmail.com>
2022-11-17 07:36:09 -0500

    third commit

--- main_course.py
+++ main_course.py
@@ -1,5 +1,4 @@
-'''
-
+"""
 Followed this github project:
 https://github.com/nicknochnack/ReinforcementLearningCourse
 https://www.youtube.com/watch?v=Mut_u40Sqz4&list=PLgNJO2hghbmid6heTu2Ar68CzJ344nUHy&index=1
@@ -13,10 +12,10 @@ Environments are represented by "spaces". Many different types of spaces. Some i
 4. Dict - dictionary of spaces (Bos or Discrete) ~ e.g. Dict({'height':Discrete(2), "speed":Box(0,100,shape=(1,))})
 5. MultiBinary - one hot encoded set of binary values ~ e.g. Multibinary(4)
 6. MultiDiscrete - multiple discrete values ~ e.g. MultiDiscrete([5,2,2])
+"""
+import os
 
-'''
-
-''' ---------- IMPORT DEPENDENCIES ---------- '''
+''' ---------- 1. IMPORT DEPENDENCIES ---------- '''
 import gym
 from stable_baselines3 import PPO
 # DummyVecEnv allows you to vectorize environments. Allows for training multiple environments at the same time.
@@ -24,7 +23,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 # evaluate_policy makes it easier to see how well model is running.
 from stable_baselines3.common.evaluation import evaluate_policy
 
-''' ---------- LOAD ENVIRONMENT ---------- '''
+''' ---------- 2. LOAD ENVIRONMENT ---------- '''
 # This is case-sensitive. This is a mapping to the pre-installed gym environment.
 environment_name = 'CartPole-v0'
 # Here, we are making our environment.
@@ -71,5 +70,57 @@ env.close()
 
 # Look at this link to understand the anatomy of the environment more. The code for all the other environments can be
 # found here.
+
+''' ---------- 3. TRAINING ---------- '''
+'''
+~ Choosing Algorithms ~
+There are a number of algorithms available through Stable Baselines. We can easily switch between these.
+Certain algorithms will perform better for certain environments. OFten it helps to review literature in order to determine the best approach.
+For a given action space it is best to choose:
+1. Discrete Single Process: DQN
+2. Discrete Multi Processed: PPO or A2C
+3. Continuous Single Process: SAC or TD3
+4. Continuous Multi Processed: PPO or A2C
+
+~ Understanding Training Metrics ~ 
+1. Evaluation Metrics: ep_len_mean - episode length mean, ep_rew_mean - episode 
+reward means 
+2. Time Metrics: fps - frames per second, iterations - how many times you've gone through, time_elapsed 
+- how long its been running in total time, total_timesteps - how many steps youve taken in an episode 
+3. Loss 
+Metrics: entropy_loss, policy_loss, value_loss 
+4. Other Metrics: explained_variance - how much environment variance the agent is able to explain, 
+learning_rate - how fast policies are updating, n_updates - total amount of updates agent has made 
+'''
+
+''' ---------- TRAIN AN RL MODEL ---------- '''
+log_path = os.path.join('Training', 'Logs')
+
+# recreate the environment
+env = gym.make(environment_name)
+# wrap our environment into the DummyVecEnv wrapper. Allows us to work with our environment while its wrapped in a
+# dummy vector environment. Think of it as a wrapper for a non-vectorized environment.
+env = DummyVecEnv([lambda: env])
+# Defining our model. Think of it as defining our agent, or even, think of it as defining the rules for how the agent
+# behaves within the environment. The first argument defines the policy that we want to use (MLP is
+# multi-layer-processing which basically means its a regular neural network). Stable Baselines 3 has three policy
+# types: MlpPolicy, CnnPolicy, and MultiInputPolicy. The second argument is the definition of the environment.
+# verbose=1 because we want to log the results of that model. then we specify our log path.
+model = PPO('MlpPolicy', env, verbose=1, tensorboard_log=log_path)
+
+model.learn(total_timesteps=20000)
+
+''' ---------- SAVE AND RELOAD MODEL ---------- '''
+# define a path to save our model to
+PPO_Path = os.path.join('Training', 'Saved_Models', 'PPO_Model_Cartpole')
+# save madel into path
+model.save(PPO_Path)
+# Delete model and then reload it. This technique kind of simulated "deployment". You're gunna be reloading from your
+# model each time you test.
+del model
+# reload model back into memory
+model = PPO.load(PPO_Path, env=env)
+
+''' ---------- 3. TESTING AND EVALUATION ---------- '''
 
 
